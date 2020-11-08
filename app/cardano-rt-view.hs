4a979459b03819fc95711c5ee688ae8875678044
Denis Shevchenko <denis.shevchenko@iohk.io>
2020-11-08 23:10:52 +0400

    CAD-2202: show its version.

--- app/cardano-rt-view.hs
+++ app/cardano-rt-view.hs
@@ -5,11 +5,14 @@ import           System.IO (hSetEncoding, stdout, stderr, utf8)
 import           System.Win32.Console (setConsoleCP)
 #endif
 
+import           Data.Version (showVersion)
 import           Options.Applicative (ParserInfo, (<**>), customExecParser, fullDesc, header,
-                                      helper, info, prefs, showHelpOnEmpty)
+                                      help, helper, info, infoOption, long, prefs, short,
+                                      showHelpOnEmpty)
 
 import           Cardano.RTView (runCardanoRTView)
 import           Cardano.RTView.CLI (RTViewParams, parseRTViewParams)
+import           Paths_cardano_rt_view (version)
 
 main :: IO ()
 main = do
@@ -24,5 +27,11 @@ main = do
   runCardanoRTView rtViewParams
  where
   rtViewInfo :: ParserInfo RTViewParams
-  rtViewInfo = info (parseRTViewParams <**> helper)
-                    (fullDesc <> header "cardano-rt-view - real-time view for cardano node.")
+  rtViewInfo = info
+    (parseRTViewParams <**> helper <**> versionOption)
+    (fullDesc <> header "cardano-rt-view - real-time view for cardano node.")
+  versionOption = infoOption
+    (showVersion version)
+    (long "version" <>
+     short 'v' <>
+     help "Show version")
